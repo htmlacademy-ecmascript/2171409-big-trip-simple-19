96acb07134819006baaca9b46ddae44f1330a1e5
imbrickiy <imbrickiy@ya.ru>
2022-12-08 17:42:53 +0300

    нарезка вёрстки на отдельные компоненты

---
+++ src/view/content-item.js
@@ -0,0 +1,22 @@
+import { createElement } from '../render.js';
+
+function createContentItem() {
+  return ('<li class="trip-events__item"></li>');
+}
+
+export default class ContentItemView {
+  getTemplate() {
+    return createContentItem();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
