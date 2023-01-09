1781b87819d35e5d83173c7341fd66361f842fa6
imbrickiy <imbrickiy@ya.ru>
2023-01-09 12:05:44 +0300

    реализовано приглашение добавить первую точку маршрута

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ import SortView from './view/sort.js';
 import ContentPresenter from './presenter/board-presenter.js';
 import { render } from './render.js';
 import PointsModel from './model/points-model.js';
-
+import FirstPointView from './view/list-empty.js';
 
 const siteMainTripElement = document.querySelector('.trip-controls');
 const siteHeaderTripElement = siteMainTripElement.querySelector('.trip-controls__filters');
 const pointsModel = new PointsModel();
 
 const siteMainElement = document.querySelector('.page-main');
+const siteBodyElement = siteMainElement.querySelector('.page-body__container');
 const siteHeaderElement = siteMainElement.querySelector('.trip-events');
 const boardPresenter = new ContentPresenter(siteHeaderElement, pointsModel);
 
+render(new FirstPointView(), siteBodyElement);
 render(new FilterView(), siteHeaderTripElement);
-render(new SortView(), siteHeaderElement);
+// render(new SortView(), siteHeaderElement);
 
-boardPresenter.init();
+// boardPresenter.init();
